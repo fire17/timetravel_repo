7708d31fe803ad2087147338d82dbf4390d4be9b
Tami Bar <fire17@gmail.com>
2022-12-19 15:44:18 +0200

    next attempts

--- passenger.py
+++ passenger.py
@@ -2,3 +2,6 @@ if __name__ == '__main__':
     # Set up the command line parser
     print("May the 4th be with you!")
     print("I sure hope this works!")
+
+    print("could not find where to edit the relevant date")
+    print("next time try to emulate a system that it's clock is set to the desired date, then upload using gpg key")
